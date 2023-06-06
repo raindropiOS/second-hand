b5c6a76cd18c586867e9e235dee3c431fb78d49b
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-06 11:54:12 +0900

    🔧 chore #9: login VC로 최초화면 설정

---
+++ ios/SecondHand/SecondHand/LoginViewController.swift
@@ -0,0 +1,15 @@
+//
+//  LoginViewController.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/06/06.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
