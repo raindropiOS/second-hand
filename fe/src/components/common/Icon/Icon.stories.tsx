85bb5f120409aa80223451d6c7339359a79eec11
JangHoonSeok <orirori512@naver.com>
2023-06-08 23:14:37 +0900

    ✅ test #28: Icon컴포넌트 스토리 추가

--- fe/src/components/common/Icon/Icon.stories.tsx
+++ fe/src/components/common/Icon/Icon.stories.tsx
@@ -10,11 +10,29 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Account: Story = {
   args: {
     name: 'account',
     width: 22,
     height: 22,
     fill: '#000000',
+  },
+};
+
+export const Camera: Story = {
+  args: {
+    name: 'camera',
+    width: 35,
+    height: 35,
+    fill: '#FF9500',
+  },
+};
+
+export const Like: Story = {
+  args: {
+    name: 'like',
+    width: 35,
+    height: 35,
+    fill: '#FF3B30',
   },
 };
