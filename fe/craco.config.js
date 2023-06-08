ccffad475d4a4964cc56df654b74203b2c5fa9df
JangHoonSeok <orirori512@naver.com>
2023-06-08 19:32:21 +0900

    🔧 chore #28: Icon 컴포넌트 위해 custom.d.ts 추가

--- fe/craco.config.js
+++ fe/craco.config.js
@@ -1,5 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires, no-undef
 const CracoAlias = require('craco-alias');
 
+// eslint-disable-next-line no-undef
 module.exports = {
   plugins: [
     {
