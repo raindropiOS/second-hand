78c29a47e26c8381206c5f444d2dbb3ed9fbba33
JaydenLee1116 <vv55adss@gmail.com>
2023-06-09 02:05:39 +0900

    🔧 chore #21: 스토리북 프리뷰 수정
    - import React가 안되는 문제 해결

--- fe/.storybook/preview.tsx
+++ fe/.storybook/preview.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme } from '../src/styles/theme';
 import GlobalStyles from '../src/styles/GlobalStyles.style';
