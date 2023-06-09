702d1108c7a71f93ab008ec86c36a33586d71606
harimrim <kigkfla@naver.com>
2023-06-09 11:08:20 +0900

    ✨ feat: #32 위치 추가 Button 구현

--- ios/SecondHand/SecondHand/SignUpViewController.swift
+++ ios/SecondHand/SecondHand/SignUpViewController.swift
@@ -8,6 +8,24 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    private let button: UIButton = {
+        let button = UIButton(type: .system)
+        let symbolConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .regular)
+        let symbolImage = UIImage(systemName: "plus", withConfiguration: symbolConfig)
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor(red: 0.702, green: 0.702, blue: 0.702, alpha: 0.39).cgColor
+        button.backgroundColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(symbolImage, for: .normal)
+        button.tintColor = .black
+        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
+        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+        button.contentEdgeInsets = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+        button.setTitle("위치 추가", for: .normal)
+        
+            return button
+        }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
