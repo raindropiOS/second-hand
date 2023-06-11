3fb97ae249ff41cc6d9bf87ec9a4b4e52526fbe7
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-12 02:40:39 +0900

    ✨ feat #33: 프로필 이미지 뷰 추가

--- ios/SecondHand/SecondHand/Profile/ProfileViewController.swift
+++ ios/SecondHand/SecondHand/Profile/ProfileViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    let profileImageView = ProfileImageView()
+    let nameLabel = UILabel()
+    let signOutButton = OrangeButton()
+    let profileImageViewSize: CGSize = CGSizeMake(100, 100)
+    
     override func viewDidLoad() {
-        
+        self.configureProfileImageView()
+        self.view.backgroundColor = .white
+    }
+    
+    private func configureProfileImageView() {
+        self.view.addSubview(profileImageView)
+        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            profileImageView.widthAnchor.constraint(equalToConstant: profileImageViewSize.width),
+            profileImageView.heightAnchor.constraint(equalToConstant: profileImageViewSize.height),
+            profileImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 219.5),
+            profileImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        ])
     }
 }
