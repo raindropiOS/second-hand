5c20f55dc0a179d47637c60ace3892ca5b0326ca
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-12 02:19:27 +0900

    ✨ feat #31: ProfileImageView 추가

---
+++ ios/SecondHand/SecondHand/Profile/ProfileImageView.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileImageView.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/06/12.
+//
+
+import UIKit.UIImageView
+
+class CircularImageView: UIImageView {
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.layer.cornerRadius = self.frame.size.height / 2
+        self.clipsToBounds = true
+    }
+    
+    func makePressable(target: UIViewController, action: Selector) {
+        self.isUserInteractionEnabled = true
+        let tapGesture = UITapGestureRecognizer(target: target, action: action)
+        tapGesture.numberOfTapsRequired = 1
+        self.addGestureRecognizer(tapGesture)
+    }
+}
