708739c9f15c2c976a07488414967f7b0a1f3aa1
JaydenLee1116 <vv55adss@gmail.com>
2023-06-11 23:27:10 +0900

    ✅ test #49: 기본 버튼 컴포넌트 스토리북 작성

---
+++ fe/src/components/common/Buttons/Button/Button.stories.tsx
@@ -0,0 +1,54 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Button from '../Button';
+import Icon from '@common/Icon';
+import { theme } from '@styles/theme';
+
+const meta = {
+  title: 'Common/Button',
+  component: Button,
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const First: Story = {
+  args: {
+    children: '대화 중인 채팅방',
+    size: 'small',
+    status: 'active',
+    justifyContent: 'center',
+  },
+  argTypes: {
+    onClick: { action: 'onClick' },
+  },
+};
+
+export const Second: Story = {
+  args: {
+    children: (
+      <>
+        <p>역삼1동</p>
+        <Icon name="cancel" width={15} height={15} fill={theme.COLORS.ACCENT.TEXT.DEFAULT} />
+      </>
+    ),
+    size: 'medium',
+    status: 'active',
+    justifyContent: 'between',
+  },
+  argTypes: {
+    onClick: { action: 'onClick' },
+  },
+};
+
+export const Third: Story = {
+  args: {
+    children: '로그인',
+    size: 'large',
+    status: 'active',
+    justifyContent: 'center',
+  },
+  argTypes: {
+    onClick: { action: 'onClick' },
+  },
+};
