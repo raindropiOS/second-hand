caebc36c2dbc8682f47cf24f27b93360304daa88
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-12 03:04:28 +0900

    ✨ feat #33: 내 계정 탭 계정정보 뷰 구현

--- ios/SecondHand/SecondHand/Profile/ProfileViewController.swift
+++ ios/SecondHand/SecondHand/Profile/ProfileViewController.swift
@@ -8,24 +8,77 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
+    let separatorViewUnderNavigationBar: SeparatorView = SeparatorView()
     let profileImageView = ProfileImageView()
     let nameLabel = UILabel()
     let signOutButton = OrangeButton()
     let profileImageViewSize: CGSize = CGSizeMake(100, 100)
     
     override func viewDidLoad() {
-        self.configureProfileImageView()
+        self.navigationItem.title = "내 계정"
         self.view.backgroundColor = .white
+        self.configureProfileImageView()
+        self.configureSeparatorViewUnderNavigationBar()
+        self.configureNameLabel()
+        self.configureSignOutButton()
+    }
+    
+    private func setName(_ name: String) {
+        self.nameLabel.text = name
+    }
+    
+    private func setImage(_ uiImage: UIImage) {
+        self.profileImageView.image = uiImage
+    }
+}
+
+// MARK: Autolayout
+extension ProfileViewController {
+    private func configureSeparatorViewUnderNavigationBar() {
+        self.view.addSubview(separatorViewUnderNavigationBar)
+        self.separatorViewUnderNavigationBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.separatorViewUnderNavigationBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.separatorViewUnderNavigationBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.separatorViewUnderNavigationBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.separatorViewUnderNavigationBar.heightAnchor.constraint(equalToConstant: SeparatorView.height)
+        ])
+        self.separatorViewUnderNavigationBar.configure()
     }
     
     private func configureProfileImageView() {
-        self.view.addSubview(profileImageView)
-        profileImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(self.profileImageView)
+        
+        self.profileImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            profileImageView.widthAnchor.constraint(equalToConstant: profileImageViewSize.width),
-            profileImageView.heightAnchor.constraint(equalToConstant: profileImageViewSize.height),
-            profileImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 219.5),
-            profileImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.profileImageView.widthAnchor.constraint(equalToConstant: profileImageViewSize.width),
+            self.profileImageView.heightAnchor.constraint(equalToConstant: profileImageViewSize.height),
+            self.profileImageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 219.5),
+            self.profileImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+        ])
+    }
+    
+    private func configureNameLabel() {
+        self.view.addSubview(self.nameLabel)
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.nameLabel.topAnchor.constraint(equalTo: profileImageView.bottomAnchor, constant: 30),
+            self.nameLabel.centerXAnchor.constraint(equalTo: profileImageView.centerXAnchor)
+        ])
+    }
+    
+    private func configureSignOutButton() {
+        let height = self.view.frame.height
+        let padding = (60/height) * height
+        self.signOutButton.setTitle("로그아웃", for: .normal)
+
+        self.view.addSubview(self.signOutButton)
+        
+        self.signOutButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.signOutButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+            self.signOutButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor)
         ])
     }
 }
