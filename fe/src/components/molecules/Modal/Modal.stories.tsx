298f4c34e25ad0d651dfefa90bee67ef6c08080a
JangHoonSeok <orirori512@naver.com>
2023-06-13 00:06:40 +0900

    ✅ test #70: 모달 컴포넌트 스토리북 작성

---
+++ fe/src/components/molecules/Modal/Modal.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Modal, { SALE_MODAL_MENUS, DETAIL_MODAL_MENUS } from '.';
+
+const meta = {
+  title: 'Common/Modal',
+  component: Modal,
+} satisfies Meta<typeof Modal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const SaleModal: Story = {
+  args: {
+    menus: SALE_MODAL_MENUS,
+  },
+
+  argTypes: {},
+};
+
+export const DetailModal: Story = {
+  args: {
+    menus: DETAIL_MODAL_MENUS,
+  },
+  argTypes: {},
+};
