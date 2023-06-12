d28b4599b44bf617a5780b653ab83302f670e606
JaydenLee1116 <vv55adss@gmail.com>
2023-06-13 00:13:59 +0900

    ✅ test #71: Textinput 컴포넌트 스토리북 작성

---
+++ fe/src/components/atoms/Inputs/TextInput/TextInput.stories.tsx
@@ -0,0 +1,48 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import TextInput from '.';
+import React from 'react';
+import { TextInputCategory } from '@atoms/Inputs/TextInput/TextInput.style';
+
+const meta = {
+  title: 'Common/TextInput',
+  component: TextInput,
+} satisfies Meta<typeof TextInput>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const First: Story = {
+  args: {
+    value: '',
+    width: 160,
+    category: 'default',
+    placeholder: '아이디를 입력하세요',
+  },
+  argTypes: {
+    onChange: { action: 'onChange' },
+  },
+};
+
+export const Second: Story = {
+  args: {
+    value: '',
+    placeholder: '글제목',
+  },
+  argTypes: {
+    onChange: { action: 'onChange' },
+  },
+};
+
+export const Third: Story = {
+  args: {
+    value: '',
+    width: 325,
+    category: 'chat',
+    placeholder: '내용을 입력하세요',
+  },
+  argTypes: {
+    onChange: { action: 'onChange' },
+    onKeyDown: { action: 'onKeyDown' },
+  },
+};
