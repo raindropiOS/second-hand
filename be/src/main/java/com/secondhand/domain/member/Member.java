d87a5c301fcbbefc164710e18e4e814f118417d6
leegyeongwhan <leekhy02@naver.com>
2023-06-14 01:50:02 +0900

    ✨ feat: 로그인을 처리해주는 member 도메인 구조를 구현
    - 로그인을 위한 api와 컨트롤러 리포지토리를 구현

---
+++ be/src/main/java/com/secondhand/domain/member/Member.java
@@ -0,0 +1,13 @@
+package com.secondhand.domain.member;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Member {
+    private String name;
+    private String number;
+}
---
+++ be/src/main/java/com/secondhand/domain/member/MemberRepository.java
@@ -0,0 +1,9 @@
+package com.secondhand.domain.member;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository{
+}
