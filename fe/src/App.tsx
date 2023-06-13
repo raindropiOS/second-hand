ebd0fcd09edf8f45ae6d8a1a3c8686db6af72f9d
JangHoonSeok <orirori512@naver.com>
2023-06-13 22:59:02 +0900

    ✨ feat #82: 라우터로 path 설정

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,16 +1,36 @@
 import React from 'react';
-import { ThemeProvider } from 'styled-components';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
+import { ThemeProvider } from 'styled-components';
 import GlobalStyles from '@styles/GlobalStyles.style';
 import { theme } from '@styles/theme';
+
+import Login from '@pages/Login';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <div>
-        <h2>Let's get started!</h2>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" />
+          <Route path="/home/town-setting" />
+          <Route path="/home/town-setting/search" />
+          <Route path="/home/category" />
+
+          <Route path="/sale" />
+          <Route path="/sale/category" />
+
+          <Route path="/product/:productId" />
+          <Route path="/product/:productId/chat/:chatId" />
+          <Route path="/product/sales" />
+          <Route path="/product/like" />
+
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/setting" />
+        </Routes>
+      </BrowserRouter>
     </ThemeProvider>
   );
 };
