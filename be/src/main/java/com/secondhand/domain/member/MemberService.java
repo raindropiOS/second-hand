6aa6f320842d8266f03da3c388c0dd993c43c262
leegyeongwhan <leekhy02@naver.com>
2023-06-14 01:54:06 +0900

    ✨ feat: 멤버의 로그인 서비스를 구현
    -  jwt 에 전달할 dto와 깃허브로부터 전달받은 화원의 dto 구현

---
+++ be/src/main/java/com/secondhand/domain/member/MemberService.java
@@ -0,0 +1,46 @@
+package com.secondhand.domain.member;
+
+import com.secondhand.dto.MemberLoginResponseDTO;
+import com.secondhand.oauth.OAuthMemberInfoDTO;
+import com.secondhand.oauth.Oauth;
+import com.secondhand.oauth.dto.AccessTokenResponseDTO;
+import com.secondhand.oauth.service.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final Logger logger = LoggerFactory.getLogger(MemberService.class);
+
+    private final MemberRepository memberRepository;
+    private final Oauth oauth;
+    private final JwtService jwtService;
+
+
+    @Transactional
+    public MemberLoginResponseDTO login(String code) throws IOException, InterruptedException {
+        //TODO  authorization code 를 받는다
+        AccessTokenResponseDTO token = oauth.getToken(code);
+        logger.debug("AccessTokenResponseDTO = {}", token);
+        OAuthMemberInfoDTO memberInfoDTO = oauth.getUserInfo(token.getAccessToken());
+        // TODO: 멤버를 저장후 보여준다
+        // Member member = memberRepository.save();
+        if (checkLoginMember(memberInfoDTO)) {
+        }
+        String jwtToken = jwtService.createToken(memberInfoDTO);
+
+        return MemberLoginResponseDTO.of(memberInfoDTO, jwtToken);
+    }
+
+    private boolean checkLoginMember(OAuthMemberInfoDTO userInfo) {
+        //TODO : 토큰을 받은 후 깃허브로 부터 받은 정보가 DB에 저장하거나 있는 정보인지 체크한다.
+        return true;
+    }
+}
