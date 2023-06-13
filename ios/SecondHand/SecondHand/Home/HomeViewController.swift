d90d7c79e90b221e1fd163b5f4028035df07d735
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-13 23:18:10 +0900

    ✨ feat #84: 홈 화면 탭 테이블 뷰 추가

--- ios/SecondHand/SecondHand/Home/HomeViewController.swift
+++ ios/SecondHand/SecondHand/Home/HomeViewController.swift
@@ -8,21 +8,24 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
+        
+        self.view.addSubview(self.tableView)
+        
+        self.setTableViewLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setTableViewLayout() {
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.tableView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
+        ])
     }
-    */
-
 }
