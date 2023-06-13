e141140740dfaca6e01c51f657c9cd67fad5812c
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-13 23:28:56 +0900

    ✨ feat #85: ProductTableViewCell 구현

--- ios/SecondHand/SecondHand/Home/ProductTableViewCell.swift
+++ ios/SecondHand/SecondHand/Home/ProductTableViewCell.swift
@@ -8,16 +8,37 @@
 import UIKit
 
 class ProductTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    static let identifier = "ProductTableViewCell"
+    private let imageSizeRatio: CGFloat = 120/361
+    
+    let productImageView = UIImageView()
+    let productNameLabel = UILabel()
+    let townNameLabel = UILabel()
+    let hoursAgoLabel = UILabel()
+    
+    private func configure() {
+        
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    private func configureLayouts() {
+        self.contentView.addSubview(productImageView)
+        self.contentView.addSubview(productNameLabel)
+        self.contentView.addSubview(townNameLabel)
+        self.contentView.addSubview(hoursAgoLabel)
+        
+        self.productImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.productNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.townNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.hoursAgoLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        guard let superview = superview else { return }
+        let imageViewLength = superview.frame.height * imageSizeRatio
+        
+        NSLayoutConstraint.activate([
+            self.productImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+            self.productImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor),
+            self.productImageView.widthAnchor.constraint(equalToConstant: imageViewLength),
+            self.productImageView.heightAnchor.constraint(equalToConstant: imageViewLength)
+        ])
     }
-
 }
