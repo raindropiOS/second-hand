78a53a0f6bd35f63767c75fe3077431d2fd14fee
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-13 20:47:03 +0900

    ✨ feat #85: ProductTableViewCell 파일 추가

---
+++ ios/SecondHand/SecondHand/Home/ProductTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProductTableViewCell.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/06/13.
+//
+
+import UIKit
+
+class ProductTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
