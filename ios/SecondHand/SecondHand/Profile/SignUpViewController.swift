ab598fc9efdf8bfc030d21f2b4b52d55fbe93975
harimrim <kigkfla@naver.com>
2023-06-14 12:18:51 +0900

    ♻️ refactor #89 : 버튼 관련 중복 코드 리팩토링
    - Button 의 NSDirectionalEdgeInsets를 private let으로 선언
    - 중복되는 코드를 setButtonUI, setButtonPadding 메서드로 분리

--- ios/SecondHand/SecondHand/Profile/SignUpViewController.swift
+++ ios/SecondHand/SecondHand/Profile/SignUpViewController.swift
@@ -8,42 +8,41 @@
 import UIKit
 
 class SignUpViewController: UIViewController {
+    private let buttonContentInsets: NSDirectionalEdgeInsets = NSDirectionalEdgeInsets(top: 16, leading: 10, bottom: 16, trailing: 10)
+    private let circleButtonContentInsets: NSDirectionalEdgeInsets = NSDirectionalEdgeInsets(top: 25.5, leading: 20.5, bottom: 25.5, trailing: 20.5)
     private let button: UIButton = {
         let button = UIButton(type: .system)
         let symbolConfig = UIImage.SymbolConfiguration(pointSize: 16, weight: .regular)
         let symbolImage = UIImage(systemName: "plus", withConfiguration: symbolConfig)
-        button.layer.cornerRadius = 8
-        button.layer.borderWidth = 1
-        button.layer.borderColor = UIColor(red: 0.702, green: 0.702, blue: 0.702, alpha: 0.39).cgColor
-        button.backgroundColor = .white
-        button.translatesAutoresizingMaskIntoConstraints = false
         button.setImage(symbolImage, for: .normal)
-        button.tintColor = .black
-        button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 10)
-        button.titleEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
-        button.contentEdgeInsets = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
         button.setTitle("위치 추가", for: .normal)
-        
-            return button
-        }()
+        return button
+    }()
+    
     private let circleButton: UIButton = {
         let circleButton = UIButton(type: .custom)
         let symbolConfig = UIImage.SymbolConfiguration(pointSize: 24, weight: .regular)
         let symbolImage = UIImage(systemName: "camera", withConfiguration: symbolConfig)
         circleButton.frame = CGRect(x: 0, y: 0, width: 80, height: 80)
-        circleButton.layer.cornerRadius = 0.5 * circleButton.bounds.size.width
-        circleButton.clipsToBounds = true
-        circleButton.translatesAutoresizingMaskIntoConstraints = false
-        circleButton.layer.borderWidth = 1
-        circleButton.layer.borderColor = UIColor(red: 0.702, green: 0.702, blue: 0.702, alpha: 0.39).cgColor
-        circleButton.backgroundColor = .white
-        circleButton.tintColor = .black
         circleButton.setImage(symbolImage, for: .normal)
-        circleButton.contentEdgeInsets = UIEdgeInsets(top: 25.5, left: 22.5, bottom: 25.5, right: 22.5)
-        
         return circleButton
     }()
-
+    
+    private func setButtonUI(button: UIButton, cornerRadius: CGFloat) {
+        button.tintColor = .white
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor(red: 0.702, green: 0.702, blue: 0.702, alpha: 0.39).cgColor
+        button.clipsToBounds = true
+        button.layer.cornerRadius = cornerRadius
+    }
+    
+    private func setButtonPadding(button: UIButton, contentInsets: NSDirectionalEdgeInsets) {
+        var config = UIButton.Configuration.filled()
+        config.contentInsets = contentInsets
+        config.imagePadding = 4
+        button.configuration = config
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
