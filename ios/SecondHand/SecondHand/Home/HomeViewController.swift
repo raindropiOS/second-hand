4b5373fc1732134dd11172fda57e53aa80390490
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-15 22:56:38 +0900

    ✨ feat #85: 홈 탭에 상품 셀 추가

--- ios/SecondHand/SecondHand/Home/HomeViewController.swift
+++ ios/SecondHand/SecondHand/Home/HomeViewController.swift
@@ -9,13 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
     let tableView = UITableView()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
         
         self.view.addSubview(self.tableView)
         
+        self.configureTableView()
         self.setTableViewLayout()
     }
     
@@ -27,5 +28,36 @@ class HomeViewController: UIViewController {
             self.tableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
             self.tableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    private func configureTableView() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+        
+        self.tableView.register(ProductTableViewCell.self, forCellReuseIdentifier: ProductTableViewCell.identifier)
+    }
+}
+
+// MARK: UITableViewDataSource
+extension HomeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ProductTableViewCell.identifier, for: indexPath) as? ProductTableViewCell else { return UITableViewCell() }
+                return cell
+    }
+}
+
+// MARK: UITableViewDelegate
+extension HomeViewController: UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        let heightAspect: CGFloat = 120.0/393.0
+        return heightAspect * self.view.frame.width
     }
 }
