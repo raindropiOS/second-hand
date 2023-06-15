c50cfc95de7d4047f24da6fdf9517ba263b54187
JaydenLee1116 <vv55adss@gmail.com>
2023-06-16 08:50:00 +0900

    🎨 style #116: 각 페이지 템플릿 스타일 컴포넌트 추가
    - 기타 스타일 수정

--- fe/src/components/molecules/Navbar/Navbar.style.ts
+++ fe/src/components/molecules/Navbar/Navbar.style.ts
@@ -6,12 +6,13 @@ interface $NavbarLayoutProps {
 }
 
 const $NavbarLayout = styled.header<$NavbarLayoutProps>`
+  position: relative;
   display: flex;
   flex-direction: row;
   justify-content: ${({ children }) => (React.Children.count(children) === 1 ? 'center' : 'space-between')};
   align-items: center;
 
-  width: 395px;
+  width: 100%;
   height: 50px;
   padding: 0 16px;
   border-bottom: 1px solid ${({ theme }) => theme.COLORS.NEUTRAL.BORDER.DEFAULT};
@@ -19,6 +20,7 @@ const $NavbarLayout = styled.header<$NavbarLayoutProps>`
   font-weight: ${({ theme }) => theme.FONT_TOKEN.HEADLINE.FONT_WEIGHT};
   font-size: ${({ theme }) => theme.FONT_TOKEN.HEADLINE.FONT_SIZE};
   line-height: ${({ theme }) => theme.FONT_TOKEN.HEADLINE.LINE_HEIGHT};
+  background-color: ${({ theme }) => theme.COLORS.NEUTRAL.BACKGROUND.DEFAULT};
 `;
 
 export { $NavbarLayout };
