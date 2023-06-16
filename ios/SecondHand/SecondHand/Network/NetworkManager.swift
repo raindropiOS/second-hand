567875d5f21ef9c195374a73e0d95d9791c9cc09
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-17 00:39:36 +0900

    ✨ feat #125: requestGithubOAuth 메소드 추가
    - 완성되지 않은 임시 메소드 입니다

--- ios/SecondHand/SecondHand/Network/NetworkManager.swift
+++ ios/SecondHand/SecondHand/Network/NetworkManager.swift
@@ -8,5 +8,34 @@
 import Foundation
 
 class NetworkManager {
+    static let shared = NetworkManager()
+    
+    func requestGithubOAuth() {
+        let clientId: String = "exampleId"
+        let urlString = "https://github.com/login/oauth/authorize"
+        let query = ["client_id": clientId]
+        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        
+        guard var urlComponents = URLComponents(string: urlString) else { return }
+        urlComponents.queryItems = queryItems
+        
+        // Github: GET
+        guard let url = urlComponents.url else { return }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "GET"
+        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if let response = response {
+                print("response : \(response)")
+                guard let data = data else { return }
+                print(String(data: data, encoding: .utf8))
+            }
+            
+            if let error = error {
+                print("error : \(error)")
+            }
+        }.resume()
+    }
     
 }
