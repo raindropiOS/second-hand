d835a4eb9d06d5ee124ec7e58bf55171f83a704e
JaydenLee1116 <vv55adss@gmail.com>
2023-06-18 22:41:06 +0900

    ♻️  refactor #116: mock 데이터 사용 로직 수정
    - mock server를 통한 통신으로 수정

--- fe/src/pages/Home/HomeMain/index.tsx
+++ fe/src/pages/Home/HomeMain/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { $Template } from '@styles/PageTemplate.style';
 import { $ListItemContainer } from '@pages/Home/HomeMain/HomeMain.style';
@@ -8,37 +8,86 @@ import Icon from '@atoms/Icon';
 import ListItem from '@molecules/ListItem';
 import MainTabBar from '@molecules/TabBars/MainTabBar';
 import ConvertPriceFormat from '@utils/convertPriceFormat';
+import mockAxiosFetch from '@apis/instances/mockAxiosFetch';
 
-import { mockData, mockUserTowns } from './mockData';
+interface Product {
+  productId: number;
+  title: string;
+  town: { townId: number; name: string };
+  createdAt: string;
+  status: string;
+  price: number;
+  countInfo: { chatCount: number; likeCount: number };
+  imgUrl: string;
+}
+
+interface Town {
+  townId: number;
+  name: string;
+}
 
 const HomeMain = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [towns, setTowns] = useState<Town[]>([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await mockAxiosFetch('/products', {
+        method: 'GET',
+      });
+      const data = await response.data;
+      const isSuccess = data.success;
+      const products = data.data.products;
+
+      // FIXME(jayden): error handling 수정하기
+      if (!isSuccess) throw new Error('Failed to fetch products');
+      setProducts(products);
+    };
+
+    getProducts();
+  }, []);
+
+  useEffect(() => {
+    const getTowns = async () => {
+      const response = await mockAxiosFetch('/towns/member', {
+        method: 'GET',
+      });
+      const data = await response.data;
+      const isSuccess = data.success;
+      const towns = data.data.town;
+
+      if (!isSuccess) throw new Error('Failed to fetch towns');
+      setTowns(towns);
+    };
+
+    getTowns();
+  }, []);
+
   return (
-    <$Template>
-      <Navbar>
-        <Dropdown towns={mockUserTowns} />
-        <button>
-          <Icon name="category" />
-        </button>
-      </Navbar>
-      <$ListItemContainer>
-        {mockData.data.products.map(product => (
-          <ListItem
-            key={product.productId}
-            productId={product.productId}
-            title={product.title}
-            town={product.town}
-            price={ConvertPriceFormat(product.price)}
-            status={product.status}
-            createdAt={product.createdAt}
-            countInfo={product.countInfo}
-            imgUrl={product.imgUrl}
-            isCurrentUserItem={true}
-            onItemClick={() => console.log('onItemClick')}
-          />
-        ))}
-      </$ListItemContainer>
-      <MainTabBar />
-    </$Template>
+    <>
+      {!!products.length && !!towns.length && (
+        <$Template>
+          <Navbar>
+            <Dropdown towns={towns} />
+            <button>
+              <Icon name="category" />
+            </button>
+          </Navbar>
+          <$ListItemContainer>
+            {products.map(product => (
+              <ListItem
+                {...product}
+                key={product.productId}
+                price={ConvertPriceFormat(product.price)}
+                isCurrentUserItem={true}
+                onItemClick={() => console.log('onItemClick')}
+              />
+            ))}
+          </$ListItemContainer>
+          <MainTabBar />
+        </$Template>
+      )}
+    </>
   );
 };
 
