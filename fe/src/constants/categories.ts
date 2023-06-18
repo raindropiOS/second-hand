ce703706f3c7a72a6a3ad06210a7d68357b22d92
JangHoonSeok <orirori512@naver.com>
2023-06-19 04:21:16 +0900

    ✨ feat #120: 카테고리 상수 생성

---
+++ fe/src/constants/categories.ts
@@ -0,0 +1,22 @@
+const CATEGORIES = [
+  { id: 1, category: '디지털기기', url: '@assets/Categories/digital.png' },
+  { id: 2, category: '생활가전', url: '@assets/Categories/domestic.png' },
+  { id: 3, category: '가구/인테리어', url: '@assets/Categories/furniture.png' },
+  { id: 4, category: '생활/주방', url: '@assets/Categories/kitchen.png' },
+  { id: 5, category: '유아동', url: '@assets/Categories/baby.png' },
+  { id: 6, category: '유아도서', url: '@assets/Categories/baby-book.png' },
+  { id: 7, category: '여성의류', url: '@assets/Categories/woman-apparel.png' },
+  { id: 8, category: '여성잡화', url: '@assets/Categories/woman-accessories.png' },
+  { id: 9, category: '남성패션/잡화', url: '@assets/Categories/man-apparel.png' },
+  { id: 10, category: '뷰티/미용', url: '@assets/Categories/beauty.png' },
+  { id: 11, category: '스포츠/레저', url: '@assets/Categories/sports.png' },
+  { id: 12, category: '취미/게임/음반', url: '@assets/Categories/game.png' },
+  { id: 13, category: '중고차', url: '@assets/Categories/car.png' },
+  { id: 14, category: '티켓/교환권', url: '@assets/Categories/ticket.png' },
+  { id: 15, category: '가공식품', url: '@assets/Categories/processed-foods.png' },
+  { id: 16, category: '반려동물용품', url: '@assets/Categories/pet.png' },
+  { id: 17, category: '식물', url: '@assets/Categories/plant.png' },
+  { id: 18, category: '기타 중고물품', url: '@assets/Categories/etc.png' },
+];
+
+export { CATEGORIES };
