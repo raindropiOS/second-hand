2978c743eedbcba4fd56a63f24835d920371f110
JangHoonSeok <orirori512@naver.com>
2023-06-20 00:31:53 +0900

    ✨ feat #153: 홈 카테고리 page 메인 UI 완성

---
+++ fe/src/components/Home/HomeCategory/HomeCategoryMain/HomeCategoryMain.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const $HomeCategoryMain = styled.main``;
+
+const $Category = styled.button``;
+
+const $CategoryImg = styled.img``;
+
+const $CategoryTitle = styled.div``;
+
+export { $HomeCategoryMain, $Category, $CategoryImg, $CategoryTitle };
--- fe/src/pages/Home/HomeCategory/index.tsx
+++ fe/src/pages/Home/HomeCategory/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import { $Template } from '@styles/PageTemplate.style';
 import HomeCategoryHeader from '@components/Home/HomeCategory/HomeCategoryHeader';
+import HomeCategoryMain from '@components/Home/HomeCategory/HomeCategoryMain';
 
 const HomeCategory = () => {
   return (
     <$Template>
       <HomeCategoryHeader />
+      <HomeCategoryMain />
     </$Template>
   );
 };
