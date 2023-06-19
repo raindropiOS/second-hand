bf8ffeed68ccb7ecbe77753d4e18f515c4f756f1
JangHoonSeok <orirori512@naver.com>
2023-06-20 00:10:36 +0900

    ✨ feat #153: 홈 카테고리 화면 헤더 UI 완성
    - navigate도 설정 완료

---
+++ fe/src/pages/Home/HomeCategory/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { $Template } from '@styles/PageTemplate.style';
+import HomeCategoryHeader from '@components/Home/HomeCategory/HomeCategoryHeader';
+
+const HomeCategory = () => {
+  return (
+    <$Template>
+      <HomeCategoryHeader />
+    </$Template>
+  );
+};
+
+export default HomeCategory;
