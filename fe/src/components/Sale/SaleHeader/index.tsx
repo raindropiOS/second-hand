edb5d4922608883db0ac22a87a46d854d8071a6c
JangHoonSeok <orirori512@naver.com>
2023-06-19 23:30:15 +0900

    ✨ feat #120: 판매 등록 페이지 뒤로가기 버튼 완성

--- fe/src/components/Sale/SaleHeader/index.tsx
+++ fe/src/components/Sale/SaleHeader/index.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import PATH from '@constants/routerPath';
 import Navbar from '@components/molecules/Navbar';
 import Button from '@atoms/Buttons/Button';
 
 const SaleHeader = () => {
   // TODO(hoonding): 완료 누르면 sessionStorage 비우기.
+  const navigate = useNavigate();
+
+  const handleBackButtonClick = () => {
+    // TODO(hoonding): sessionStorage에 작성중이던 form 저장하기.
+    navigate(PATH.HOME.DEFAULT);
+  };
+
   return (
     <Navbar>
-      <Button onClick={() => console.log('second-test')} status="ghost">
+      <Button onClick={handleBackButtonClick} status="ghost">
         <span>뒤로</span>
       </Button>
       <span>내 물건 팔기</span>
