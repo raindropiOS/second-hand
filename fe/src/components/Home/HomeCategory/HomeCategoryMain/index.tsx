a0e71429564a5eced4b9e7977bf82ce7d1a5007c
JangHoonSeok <orirori512@naver.com>
2023-06-20 00:32:14 +0900

    ✨ feat #153: 홈 카테고리 페이지 UI 완성

---
+++ fe/src/components/Home/HomeCategory/HomeCategoryMain/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { CATEGORIES } from '@constants/categories';
+import { $HomeCategoryMain, $Category, $CategoryImg, $CategoryTitle } from './HomeCategoryMain.style';
+
+const HomeCategoryMain = () => {
+  return (
+    <$HomeCategoryMain>
+      {CATEGORIES.map(({ id, category, url }) => (
+        <$Category key={id}>
+          <$CategoryImg src={url} />
+          <$CategoryTitle>{category}</$CategoryTitle>
+        </$Category>
+      ))}
+    </$HomeCategoryMain>
+  );
+};
+
+export default HomeCategoryMain;
