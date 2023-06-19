1ce593b8ac00d4131ec9298ab47dcdbd4ab374aa
leegyeongwhan <leekhy02@naver.com>
2023-06-19 22:35:07 +0900

    ✨ feat: 이미지 업로드 도메인 구현
    - s3에 이미지를 업로드하기 위해 Config 설정, 서비스 컨트롤러를 구현
    S3Client 객체를 이용해 업로드한다.

---
+++ be/src/main/java/com/secondhand/web/contoroller/ImageResponse.java
@@ -0,0 +1,10 @@
+package com.secondhand.web.contoroller;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class ImageResponse {
+    private final String image;
+}
---
+++ be/src/main/java/com/secondhand/web/contoroller/ImageService.java
@@ -0,0 +1,76 @@
+package com.secondhand.web.contoroller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.*;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+@PropertySource("classpath:application-aws.yml")
+public class ImageService {
+
+    public static final String FILE_ROUTE = "images/test/";
+    private final S3Client s3Client;
+
+    @Value("${aws.s3.bucket}")
+    private String bucket;
+
+    public ImageResponse upload(MultipartFile multipartFile) throws IOException {
+        String imageName = getImageName(multipartFile);
+        String fileName = FILE_ROUTE + multipartFile.getOriginalFilename();
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileName)
+                .build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(multipartFile.getBytes()));
+
+        String imageUrl = s3Client.utilities().getUrl(GetUrlRequest.builder()
+                        .bucket(bucket)
+                        .key(fileName)
+                        .build())
+                .toString();
+
+        return new ImageResponse(imageUrl);
+    }
+
+    public String uploads(MultipartFile multipartFile) throws IOException {
+        String imageName = getImageName(multipartFile);
+        String fileName = FILE_ROUTE + multipartFile.getOriginalFilename();
+
+        log.debug("fileName = {} ", fileName);
+        log.debug("multipartFile = {} ", imageName);
+
+        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(fileName)
+                .build();
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(multipartFile.getBytes()));
+
+        String imageUrl = s3Client.utilities().getUrl(GetUrlRequest.builder()
+                        .bucket(bucket)
+                        .key(fileName)
+                        .build())
+                .toString();
+
+        log.debug("imageUrl = {} ", imageUrl);
+        return imageUrl;
+    }
+
+
+    private String getImageName(MultipartFile multipartFile) {
+        String newName = UUID.randomUUID().toString();
+        String originalFileName = multipartFile.getOriginalFilename();
+        String extension = originalFileName.substring(originalFileName.lastIndexOf('.'));
+        return newName + extension;
+    }
+}
