06a153655480206aee4b1796c105aeed6dc553da
kimdongmin <meena2003@naver.com>
2023-06-19 16:36:36 +0900

    🎨 style : Category 속성 이름 추가
    - imgUrl 에노테이션에 name = 'img url' 추가

--- be/src/main/java/com/secondhand/domain/categorie/Category.java
+++ be/src/main/java/com/secondhand/domain/categorie/Category.java
@@ -15,6 +15,6 @@ public class Category {
     @Column(length = 45, nullable = false)
     private String name;
 
-    @Column(length = 200, nullable = false)
+    @Column(name = "img_url", length = 200, nullable = false)
     private String imgUrl;
 }
