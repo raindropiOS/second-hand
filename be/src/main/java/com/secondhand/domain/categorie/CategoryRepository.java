147123f8d29eed1bc0afd46ff41cc3612be51092
kimdongmin <meena2003@naver.com>
2023-06-19 16:39:12 +0900

    ✨ feat: CategoryRepository 파일 생성

--- be/src/main/java/com/secondhand/domain/categorie/CategoryRepository.java
+++ be/src/main/java/com/secondhand/domain/categorie/CategoryRepository.java
@@ -1,4 +1,8 @@
 package com.secondhand.domain.categorie;
 
-public interface CategoryRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
