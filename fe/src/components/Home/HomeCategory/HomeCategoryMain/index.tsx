2143fccbd103e38254318b1df0b443e73dd5bc15
JangHoonSeok <orirori512@naver.com>
2023-06-20 14:24:21 +0900

    💄 style #153: 카테고리 png 경로 변경

--- fe/src/components/Home/HomeCategory/HomeCategoryMain/index.tsx
+++ fe/src/components/Home/HomeCategory/HomeCategoryMain/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import { CATEGORIES } from '@constants/categories';
+
 import { $HomeCategoryMain, $Category, $CategoryImg, $CategoryTitle } from './HomeCategoryMain.style';
 
 const HomeCategoryMain = () => {
