b263d1162a070932296a408d8b7ed0b3756a9487
leegyeongwhan <leekhy02@naver.com>
2023-06-21 19:23:30 +0900

    ✨ feat: 회원의 id를 가져오기 위해 수정

--- be/src/main/java/com/secondhand/oauth/service/JwtService.java
+++ be/src/main/java/com/secondhand/oauth/service/JwtService.java
@@ -13,7 +13,9 @@ import java.util.Date;
 @Service
 public class JwtService {
 
-    private String secret; // 시크릿 키를 설정
+    public static final String SUBJECT_NAME = "login_member";
+    public static final String USER_ID = "userId";
+    private String secret;
 
     public JwtService(@Value("${JWT_SECRET_KEY}") String secret) {
         this.secret = secret;
@@ -21,29 +23,32 @@ public class JwtService {
 
     public String createToken(OAuthMemberInfoDTO memberInfo) {
         return Jwts.builder()
-                .setSubject("login_member")
-                .claim("memberInfo", memberInfo) //페이로드,헤더는 자동설정
+                .setSubject(SUBJECT_NAME)
+                .claim(USER_ID, memberInfo.getId()) //페이로드,헤더는 자동설정
                 .setExpiration(new Date((new Date()).getTime() + 24 * 60 * 60 * 1000)) // 토큰의 만료일을 설정 : 현재 1일
                 .signWith(SignatureAlgorithm.HS256, secret) // HS256 알고리즘과 시크릿 키를 사용하여 서명
-                .compact(); // 토큰을 생성하세요.
+                .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            // 토큰 검증
             Jws<Claims> claimsJws = Jwts.parser()
                     .setSigningKey(secret)
                     .parseClaimsJws(token);
 
-            // 토큰이 만료되었는지 확인
             if (claimsJws.getBody().getExpiration().before(new Date())) {
-                return false; // 토큰이 만료되었습니다.
+                return false;
             }
 
-            return true; // 토큰이 유효합니다.
+            return true;
 
         } catch (Exception e) {
-            return false; // 토큰 검증에 실패했습니다.
+            return false;
         }
+    }
+
+    public Long getSubject(String token) {
+        Claims claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+        return claims.get("userId", Long.class);
     }
 }
