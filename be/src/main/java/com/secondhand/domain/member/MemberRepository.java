1726b663730abdd695d1b82a7b3f4c4a89bdd8af
leegyeongwhan <leekhy02@naver.com>
2023-06-22 03:57:41 +0900

    ✨ feat: 깃허브 login 된 사용자로인한 변경

--- be/src/main/java/com/secondhand/domain/member/MemberRepository.java
+++ be/src/main/java/com/secondhand/domain/member/MemberRepository.java
@@ -2,8 +2,14 @@ package com.secondhand.domain.member;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    @Query("SELECT m FROM Member m WHERE m.memberName = :name")
+    Optional<Member> findByMemberName(String name);
 }
