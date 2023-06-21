efbddd303dd56cf3a7111872c4a2f768bd6631fe
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-06-21 23:34:17 +0900

    ✨ feat #175: makeUrlRequest 수정
    - query를 받을 수 있도록 수정

--- ios/SecondHand/SecondHand/Network/URLRequestFactory.swift
+++ ios/SecondHand/SecondHand/Network/URLRequestFactory.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 class URLRequestFactory: URLRequestProducible {
-    func makeUrlRequest(_ urlString: String) -> URLRequest? {
+    func makeUrlRequest(_ urlString: String, query: [String: String] = [:]) -> URLRequest? {
         guard let url = URL(string: urlString) else { return nil }
-        return URLRequest(url: url, timeoutInterval: 30.0)
+        guard var urlComponents = URLComponents(string: urlString) else { return nil }
+        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        urlComponents.queryItems = queryItems
+        guard let newUrl = urlComponents.url else { return nil }
+        let request = URLRequest(url: newUrl, timeoutInterval: 30.0)
+        return request
     }
 }
 
 protocol URLRequestProducible {
-    func makeUrlRequest(_ urlString: String) -> URLRequest?
+    func makeUrlRequest(_ urlString: String, query: [String: String]) -> URLRequest?
 }
