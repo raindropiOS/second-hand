c57fae9f323559775d2292dacb4c2f0fe4b27940
JaydenLee1116 <vv55adss@gmail.com>
2023-06-21 23:57:27 +0900

    ✨ feat #173: Header에 SegmentedControl 컴포넌트 적용

---
+++ fe/src/components/SalesHistory/SalesHistoryHeader/SalesHistoryHeader.style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const $SegmentedControlContainer = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+`;
+
+export { $SegmentedControlContainer };
--- fe/src/components/SalesHistory/SalesHistoryHeader/index.tsx
+++ fe/src/components/SalesHistory/SalesHistoryHeader/index.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
+
 import Navbar from '@molecules/Navbar';
+import SegmentedControl from '@molecules/SegmentedControl';
+import { $SegmentedControlContainer } from '@components/SalesHistory/SalesHistoryHeader/SalesHistoryHeader.style';
+
+const SALES_STATUSES = [
+  { id: 0, title: '판매중' },
+  { id: 2, title: '판매완료' },
+];
 
 const SalesHistoryHeader = () => {
   return (
-    <Navbar>
-      <span>판매 내역</span>
-    </Navbar>
+    <>
+      <Navbar>
+        <span>판매 내역</span>
+      </Navbar>
+      <$SegmentedControlContainer>
+        <SegmentedControl>
+          {SALES_STATUSES.map((status, index) => (
+            <SegmentedControl.SegmentedButton
+              key={status.id}
+              segmentedIndex={index}
+              onClick={() => {
+                console.log(status.id);
+              }}
+            >
+              {status.title}
+            </SegmentedControl.SegmentedButton>
+          ))}
+        </SegmentedControl>
+      </$SegmentedControlContainer>
+    </>
   );
 };
 
