48c668a103003feeed47be7e050493c8cecb3dba
leegyeongwhan <leekhy02@naver.com>
2023-06-22 18:18:19 +0900

    ✨ feat: 프론트와 상의하에 code body로 보내도록변경

---
+++ be/src/main/java/com/secondhand/web/dto/requset/RequestCode.java
@@ -0,0 +1,10 @@
+package com.secondhand.web.dto.requset;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RequestCode {
+    private final String code;
+}
