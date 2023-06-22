18967a0d191bcb0698aae80f753590b9c4e6dc9d
JaydenLee1116 <vv55adss@gmail.com>
2023-06-22 16:57:14 +0900

    ✨ feat #173: 판매 목록 페이지 스타일 컴포넌트 구현

--- fe/src/components/SalesHistory/SalesHistoryHeader/SalesHistoryHeader.style.ts
+++ fe/src/components/SalesHistory/SalesHistoryHeader/SalesHistoryHeader.style.ts
@@ -1,12 +1,23 @@
 import styled from 'styled-components';
 
+const $SalesHistoryHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const $SegmentedControlContainer = styled.section`
+  position: fixed;
   display: flex;
   flex-direction: row;
   justify-content: center;
   align-items: center;
-
   width: 100%;
+  max-width: 768px;
+  top: 60px;
+
+  z-index: 9999;
 `;
 
-export { $SegmentedControlContainer };
+export { $SalesHistoryHeaderContainer, $SegmentedControlContainer };
