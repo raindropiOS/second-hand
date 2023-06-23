b59a0d0ce6addb6e0ddb7e9abaaffc21b64c5465
harimrim <kigkfla@naver.com>
2023-06-23 12:21:27 +0900

    ✨ feat #211 : 검색 로직 구현
    - searchCompeleter delegate 설정
    - resultTypes 를 address로 설정
    - searchBar의 searchText를 QueryFragment로 넘겨주는 메서드 구현
    - searchBar의 자동완성을 위한 UIsearchResultsUpdating protocol을 extension으로 구현
    - MKLocalsearchCompeleterDelegate 프로토콜을 extension으로 구현

--- ios/SecondHand/SecondHand/Home/TownSearchViewController.swift
+++ ios/SecondHand/SecondHand/Home/TownSearchViewController.swift
@@ -28,6 +28,16 @@ class TownSearchViewController: UIViewController, UISearchControllerDelegate, UI
         // Do any additional setup after loading the view.
     }
     
+    func setupSearchCompleter() {
+        searchCompleter.delegate = self
+        searchCompleter.resultTypes = .address
+        }
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+            // searchText를 queryFragment로 넘겨준다.
+            searchCompleter.queryFragment = searchText
+        }
+}
 
     /*
     // MARK: - Navigation
@@ -36,7 +46,22 @@ class TownSearchViewController: UIViewController, UISearchControllerDelegate, UI
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+extension TownSearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        searchBar(self.searchBarController.searchBar, textDidChange: self.searchBarController.searchBar.text ?? "")
     }
     */
 
+extension TownSearchViewController: MKLocalSearchCompleterDelegate {
+    // 자동완성 완료 시에 결과를 받는 함수
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        // completer.results를 통해 검색한 결과를 searchResults에 담아줍니다
+        searchResults = completer.results
+        self.tableView.reloadData()
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        // 에러 확인
+        print(error.localizedDescription)
+    }
 }
