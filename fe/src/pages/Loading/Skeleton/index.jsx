3dd21c32e568ba9d9c66b3c5b1fd942b0ed58870
JangHoonSeok <orirori512@naver.com>
2023-06-24 23:47:03 +0900

    ✨ feat #81: 스켈레톤 로딩 화면 하단바는 보이게끔 변경

--- fe/src/pages/Loading/Skeleton/index.jsx
+++ fe/src/pages/Loading/Skeleton/index.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 
+import { $Template } from '@styles/PageTemplate.style';
+
 import SkeletonListItem from '@components/Loading/Skeleton/SkeletonListItem';
 import SkeletonHeader from '@components/Loading/Skeleton/SkeletonHeader';
+import { $MainLayout } from './Skeleton.style';
 
 const Skeleton = () => {
   return (
     <>
       <SkeletonHeader />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
-      <SkeletonListItem />
+      <$MainLayout>
+        <SkeletonListItem />
+        <SkeletonListItem />
+        <SkeletonListItem />
+        <SkeletonListItem />
+      </$MainLayout>
     </>
   );
 };
