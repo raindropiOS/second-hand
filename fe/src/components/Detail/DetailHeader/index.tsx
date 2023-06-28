c9f7b4b78f05244cdde9871e9dfcbdfe95ae20d6
JaydenLee1116 <vv55adss@gmail.com>
2023-06-28 20:33:19 +0900

    ✨ feat #218: 상품 상세 header, main data 로직 추가
    - DetailProduct 타입 추가

--- fe/src/components/Detail/DetailHeader/index.tsx
+++ fe/src/components/Detail/DetailHeader/index.tsx
@@ -3,33 +3,32 @@ import React from 'react';
 import Navbar from '@molecules/Navbar';
 import Icon from '@atoms/Icon';
 import Carousel from '@molecules/Carousel';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Button from '@atoms/Buttons/Button';
 
-const DetailHeader = () => {
+interface DetailHeaderProps {
+  imgUrls?: string[];
+}
+
+const DetailHeader = ({ imgUrls }: DetailHeaderProps) => {
+  const navigate = useNavigate();
+
   return (
     <>
       <Navbar isTransparent={true}>
-        <Link to="/home">
+        <Button onClick={() => navigate(-1)} status="ghost">
           <Icon name="chevronLeft" />
-        </Link>
+        </Button>
         <Button onClick={() => console.log('modal page')} status="ghost">
           <Icon name="more" />
         </Button>
       </Navbar>
       <Carousel>
-        <Carousel.Slide>
-          <img
-            src="https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80"
-            alt="carousel"
-          />
-        </Carousel.Slide>
-        <Carousel.Slide>
-          <img
-            src="https://images.unsplash.com/photo-1593134257782-e89567b7718a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=735&q=80"
-            alt="carousel"
-          />
-        </Carousel.Slide>
+        {imgUrls?.map((url, i) => (
+          <Carousel.Slide key={i}>
+            <img src={url} alt="carousel" />
+          </Carousel.Slide>
+        ))}
       </Carousel>
     </>
   );
--- fe/src/components/Detail/DetailMain/index.tsx
+++ fe/src/components/Detail/DetailMain/index.tsx
@@ -12,28 +12,43 @@ import {
   $ProductSubInfoWrapper,
 } from './DetailMain.style';
 
-const DetailMain = () => {
+import { DetailProductType } from '@type/productsType';
+import GetStatusWord from '@utils/getStatusWord';
+
+interface DetailMainProps {
+  productDetail?: DetailProductType;
+}
+
+const DetailMain = ({ productDetail }: DetailMainProps) => {
   return (
-    <$DetailMainContainer>
-      <$SellerInfoWrapper>
-        <div>판매자 정보</div>
-        <div>판매자 닉네임</div>
-      </$SellerInfoWrapper>
-      <$ProductInfoContainer>
-        <$ProductStatusButton>
-          <span>판매중</span>
-          <Icon name="chevronDown" width={12} />
-        </$ProductStatusButton>
-        <$ProductTitle>상품 제목</$ProductTitle>
-        <$ProductPriceTimestamp>가구/인테리어•1분 전</$ProductPriceTimestamp>
-        <$ProductDescription>상품 설명</$ProductDescription>
-        <$ProductSubInfoWrapper>
-          <span>채팅 0</span>
-          <span>관심 0</span>
-          <span>조회 1</span>
-        </$ProductSubInfoWrapper>
-      </$ProductInfoContainer>
-    </$DetailMainContainer>
+    <>
+      {productDetail ? (
+        <$DetailMainContainer>
+          <$SellerInfoWrapper>
+            <div>판매자 정보</div>
+            <div>{`${productDetail?.seller.name}`}</div>
+          </$SellerInfoWrapper>
+          <$ProductInfoContainer>
+            <$ProductStatusButton>
+              <span>{`${GetStatusWord(productDetail?.status as number)}`}</span>
+              <Icon name="chevronDown" width={12} />
+            </$ProductStatusButton>
+            <$ProductTitle>{`${productDetail?.title}`}</$ProductTitle>
+            <$ProductPriceTimestamp>
+              {`${productDetail?.category.name}`}•{`${productDetail?.createdAt}`}
+            </$ProductPriceTimestamp>
+            <$ProductDescription>{`${productDetail?.content}`}</$ProductDescription>
+            <$ProductSubInfoWrapper>
+              <span>채팅 {`${productDetail?.countInfo.chatCount}`}</span>
+              <span>관심 {`${productDetail?.countInfo.likeCount}`}</span>
+              <span>조회 {`${productDetail?.countInfo.viewCount}`}</span>
+            </$ProductSubInfoWrapper>
+          </$ProductInfoContainer>
+        </$DetailMainContainer>
+      ) : (
+        <div>로딩 중</div>
+      )}
+    </>
   );
 };
 
