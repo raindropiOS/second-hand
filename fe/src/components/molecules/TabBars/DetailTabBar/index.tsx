2c1b7c4565cd5a8fbadc1de507afe15b5fa94dda
JangHoonSeok <orirori512@naver.com>
2023-06-30 03:09:24 +0900

    ✨ feat #81: 상품 상세 페이지 patch요청 및 isMine일때 상품 status 변경하는 component 없애기

--- fe/src/components/molecules/TabBars/DetailTabBar/index.tsx
+++ fe/src/components/molecules/TabBars/DetailTabBar/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { changeLikeStatus } from '@apis/api/productDetail';
 import convertPriceFormat from '@utils/convertPriceFormat';
 
 import { $DetailTabBar, $LikeLayout } from './DetailTabBar.style';
@@ -8,13 +9,25 @@ import Button from '@atoms/Buttons/Button';
 
 interface DetailTabBarProps {
   price?: number;
+  isLiked: boolean | undefined;
+  productId: string;
+  refetch: any;
 }
 
-const DetailTabBar = ({ price }: DetailTabBarProps) => {
+const DetailTabBar = ({ price, isLiked, productId, refetch }: DetailTabBarProps) => {
+  const handleLikeClick = async (isLiked: boolean, productId: string) => {
+    changeLikeStatus(productId, isLiked);
+    await refetch();
+  };
+
   return (
     <$DetailTabBar>
-      <$LikeLayout>
-        <Icon name="like" width={24} height={24} />
+      <$LikeLayout onClick={() => handleLikeClick(isLiked as boolean, productId)}>
+        {isLiked ? (
+          <Icon name="fillHeart" width={24} height={24} fill="#FF9500" />
+        ) : (
+          <Icon name="like" width={24} height={24} />
+        )}
         {convertPriceFormat(price || 0)}
       </$LikeLayout>
       <Button onClick={() => console.log('chat button')} status="active" size="small">
