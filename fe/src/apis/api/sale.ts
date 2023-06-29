6e002baa7909f9ecb0a8bd6656424bd3a59ac4be
JangHoonSeok <orirori512@naver.com>
2023-06-29 18:19:41 +0900

    ✨ feat #81: fetch하는 option 변경

--- fe/src/apis/api/sale.ts
+++ fe/src/apis/api/sale.ts
@@ -5,9 +5,10 @@ import { PRODUCTS } from '@constants/API';
 import { LikeProductsType, APIDefaultResponseType } from '@type/productsType';
 // TODO(hoonding) : axiosFetch로 변경해야함
 import mockAxiosFetch from '../instances/mockAxiosFetch';
+import axiosFetch from '@apis/instances/axiosFetch';
 
 const postNewProduct = async (formData: FormData) => {
-  const response = await mockAxiosFetch.post(PRODUCTS.POST_NEW_PRODUCT, formData);
+  const response = await axiosFetch.post(PRODUCTS.POST_NEW_PRODUCT, formData);
 
   return response.data;
 };
