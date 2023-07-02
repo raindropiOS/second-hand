50f4848235e215c2049b6d419e04da5acc9c1eb8
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-02 18:24:42 +0900

    ♻️  refactor #152: ProductListViewModel에 repository 추가
    - productRepository 추가

--- ios/SecondHand/SecondHand/ViewModel/ProductListViewModel.swift
+++ ios/SecondHand/SecondHand/ViewModel/ProductListViewModel.swift
@@ -10,30 +10,33 @@ import Foundation
 
 /// 상품 목록을 나타내는 뷰에 필요한 정보만을 담은 ProductViewModel을 배열로 가질 객체
 class ProductListViewModel {
-    @Published var products: [Product]
-    var cancellables = Set<AnyCancellable>()
+    private var productRepository: ProductRepository
+    /// ProductListViewModel가 할당해제 될 때 자동적으로 이 프라퍼티가 할당해제 되고, 구독 해제됨.
+    private var subscriptions = Set<AnyCancellable>()
     @Published var productViewModels: [ProductViewModel] = []
-    
-    init(products: [Product]) {
-        self.products = products
-        
-        self.$products
-            .sink { [weak self] _ in
-                // 강한 순환 참조를 피하기 위해 캡쳐리스트 사용
-                // 값이 변경될 때 수행할 작업
-                self?.loadProductViewModels()
-                self?.updateView()
-            }
-            .store(in: &cancellables)
+    @Published private var products: [Product] {
+        didSet {
+            let productViewModels = convertProductToProductViewModel(self.products)
+            self.productViewModels = productViewModels
+        }
     }
     
-    private func loadProductViewModels() {
-        let productViewModels = self.convertProductToProductViewModel()
-        self.productViewModels = productViewModels
+    init(productRepository: ProductRepository) {
+        let products = productRepository.readAll()
+        
+        self.products = products
+        self.productRepository = productRepository
+        
+        self.productRepository.objectWillChange
+            .sink { [weak self] _ in
+                let products = productRepository.readAll()
+                self?.products = products
+            }
+            .store(in: &subscriptions)
     }
     
     /// DTO이자 모델에 해당하는 Product를 ViewModel로 변경하는 메소드
-    private func convertProductToProductViewModel() -> [ProductViewModel] {
+    private func convertProductToProductViewModel(_ products: [Product]) -> [ProductViewModel] {
         self.products.map { product in ProductViewModel(product: product) }
     }
     
