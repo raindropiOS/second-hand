34b1acd34f1ce073f48fda7feee1dc7d6e7408c2
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-08 20:34:47 +0900

    🎨 style #152: 주석 추가 및 loadImage 메소드 수정
    - 이해하기 쉽게 파라미터명 변경 및 주석 추가

--- ios/SecondHand/SecondHand/Network/ImageCacheManager.swift
+++ ios/SecondHand/SecondHand/Network/ImageCacheManager.swift
@@ -15,8 +15,9 @@ class ImageCacheManager {
 }
 
 extension UIImageView {
-    func loadImage(_ imageID: String) {
-        let cacheKey = NSString(string: imageID)
+    /// imageUrlString을 이미지 cacheKey로 사용
+    func loadImage(_ imageUrlString: String) {
+        let cacheKey = NSString(string: imageUrlString)
         // 메모리 캐시에서 이미지 검색
         if let cachedImage = ImageCacheManager.shared.object(forKey: cacheKey) {
         self.image = cachedImage
@@ -24,7 +25,7 @@ extension UIImageView {
         }
         
         // url에서 이미지를 비동기 로드
-        NetworkManager.shared.loadImage(from: imageID) { image in
+        NetworkManager.shared.loadImage(from: imageUrlString) { image in
             guard let image = image else { return }
             ImageCacheManager.shared.setObject(image, forKey: cacheKey)
             DispatchQueue.main.async {
