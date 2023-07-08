6a49346c73e5af32c88c336ac41cf6ba92324a3a
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-08 21:46:15 +0900

    ✨ feat #259: Github Client ID 저장
    - .xcconfig 및 plist 수정

--- ios/SecondHand/SecondHand/Profile/SignInViewController.swift
+++ ios/SecondHand/SecondHand/Profile/SignInViewController.swift
@@ -38,7 +38,7 @@ class SignInViewController: UIViewController {
     }
     
     @objc func loginButtonAction() {
-        let clientId: String = "ClientId"
+        let clientId: String = Bundle.main.object(forInfoDictionaryKey: "githubClientId") as? String ?? ""
         let urlStr = "https://github.com/login/oauth/authorize?client_id=\(clientId)"
         guard let url = URL(string: urlStr) else { return }
         UIApplication.shared.open(url)
