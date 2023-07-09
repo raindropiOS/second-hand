7955343dc5e1211606eacb56de4dd4ee46d58f17
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-09 16:45:01 +0900

    ✨ feat #259: URLRequest 만드는 메소드 추가
    - 헤더와 바디를 수정할 수 있는 메소드 추가

--- ios/SecondHand/SecondHand/Network/URLRequestFactory.swift
+++ ios/SecondHand/SecondHand/Network/URLRequestFactory.swift
@@ -18,10 +18,33 @@ class URLRequestFactory: URLRequestProducible {
         request.httpMethod = httpMethod.rawValue
         return request
     }
+    
+    func makeUrlRequest(_ urlString: String, query: [String: String], header: [String: String], body: String, httpMethod: HttpMethod) throws -> URLRequest {
+        guard var urlComponents = URLComponents(string: urlString) else { throw URLRequestError.badUrlComponents  }
+        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
+        urlComponents.queryItems = queryItems
+        guard let url = urlComponents.url else { throw URLRequestError.badUrl }
+        var request = URLRequest(url: url, timeoutInterval: 30.0)
+        
+        // 헤더 수정
+        for (key, value) in header {
+            request.setValue(key, forHTTPHeaderField: value)
+        }
+        
+        // 바디 수정
+        let bodyData = body.data(using: .utf8)
+        request.httpBody = bodyData
+        
+        // 메소드 수정
+        request.httpMethod = httpMethod.rawValue
+        return request
+    }
 }
 
 protocol URLRequestProducible {
     func makeUrlRequest(_ urlString: String, query: [String: String], httpMethod: HttpMethod) throws -> URLRequest
+    
+    func makeUrlRequest(_ urlString: String, query: [String: String], header: [String: String], body: String, httpMethod: HttpMethod) throws -> URLRequest
 }
 
 enum URLRequestError: Error {
