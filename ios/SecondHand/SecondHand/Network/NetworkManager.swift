c3d6ca6ae34e6653de28e02dea66ec7330d81fee
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-10 16:10:07 +0900

    🔥 remove #259: 미사용 메소드 삭제

--- ios/SecondHand/SecondHand/Network/NetworkManager.swift
+++ ios/SecondHand/SecondHand/Network/NetworkManager.swift
@@ -70,35 +70,6 @@ class NetworkManager: NetworkManageable {
             print("error : \(error)")
         }
     }
-    
-    func requestGithubOAuth() {
-        let clientId: String = "exampleId"
-        let urlString = "https://github.com/login/oauth/authorize"
-        let query = ["client_id": clientId]
-        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
-        
-        guard var urlComponents = URLComponents(string: urlString) else { return }
-        urlComponents.queryItems = queryItems
-        
-        // Github: GET
-        guard let url = urlComponents.url else { return }
-        var urlRequest = URLRequest(url: url)
-        urlRequest.httpMethod = "GET"
-        urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            if let response = response {
-                print("response : \(response)")
-                guard let data = data else { return }
-                print(String(data: data, encoding: .utf8))
-            }
-            
-            if let error = error {
-                print("error : \(error)")
-            }
-        }.resume()
-    }
-    
 }
 
 // TODO: GitHub OAuth
