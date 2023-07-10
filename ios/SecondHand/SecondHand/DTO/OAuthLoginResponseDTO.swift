8a2253c87d439233dbb173fcedb2c268afbd0ce3
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-10 15:54:00 +0900

    ✨ feat #259: OAuthLoginResponseDTO 추가
    - GitHub OAuth 로그인으로 받는 DTO 추가

---
+++ ios/SecondHand/SecondHand/DTO/OAuthLoginResponseDTO.swift
@@ -0,0 +1,22 @@
+//
+//  OAuthLoginResponseDTO.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/07/10.
+//
+
+import Foundation
+
+struct OAuthLoginResponseDTO {
+    let success: Bool
+    let httpStatus: String
+    let apiStatus: Int
+    let data: UserInfoData
+    let message: String
+}
+
+struct UserInfoData {
+    let name: String
+    let imgUrl: String
+    let jwtToken: String
+}
