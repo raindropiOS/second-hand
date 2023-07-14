6d893f79dc06afa800222989ef4abb3b8aa9567b
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-14 22:33:53 +0900

    ♻️  refactor #259: URLRequestProducible 메소드 수정
    - 불필요해진 메소드 삭제

--- ios/SecondHand/SecondHand/Network/URLRequestProducible.swift
+++ ios/SecondHand/SecondHand/Network/URLRequestProducible.swift
@@ -8,45 +8,10 @@
 import Foundation
 
 protocol URLRequestProducible {
-    func makeUrlRequest(_ urlString: String, query: [String: String], httpMethod: HttpMethod) throws -> URLRequest
-    
-    func makeUrlRequest(_ urlString: String, query: [String: String], header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest
-    
     func makeUrlRequest(_ urlComponents: URLComponents, header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest
 }
 
 extension URLRequestProducible {
-    func makeUrlRequest(_ urlString: String, query: [String: String], httpMethod: HttpMethod) throws -> URLRequest {
-        guard var urlComponents = URLComponents(string: urlString) else { throw URLRequestError.badUrlComponents  }
-        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
-        urlComponents.queryItems = queryItems
-        guard let url = urlComponents.url else { throw URLRequestError.badUrl }
-        var request = URLRequest(url: url, timeoutInterval: 30.0)
-        request.httpMethod = httpMethod.rawValue
-        return request
-    }
-    
-    func makeUrlRequest(_ urlString: String, query: [String: String], header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest {
-        guard var urlComponents = URLComponents(string: urlString) else { throw URLRequestError.badUrlComponents  }
-        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
-        urlComponents.queryItems = queryItems
-        guard let url = urlComponents.url else { throw URLRequestError.badUrl }
-        var request = URLRequest(url: url, timeoutInterval: 30.0)
-        
-        // 헤더 수정
-        for (key, value) in header {
-            request.setValue(value, forHTTPHeaderField: key)
-        }
-        
-        // 바디 수정
-        let jsonData = try? JSONSerialization.data(withJSONObject: body)
-        request.httpBody = jsonData
-        
-        // 메소드 수정
-        request.httpMethod = httpMethod.rawValue
-        return request
-    }
-    
     func makeUrlRequest(_ urlComponents: URLComponents, header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest {
         guard let url = urlComponents.url else { throw URLRequestError.badUrl }
         var urlRequest = URLRequest(url: url, timeoutInterval: 5.0)
