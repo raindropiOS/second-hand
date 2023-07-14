a50915484d25817e59cca534ad47520dcbff5435
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-14 22:05:19 +0900

    ✨ feat #259: 메소드 분리를 위한 makeUrlRequest 메소드 추가
    - URLComponents를 받는 메소드 추가

--- ios/SecondHand/SecondHand/Network/URLRequestProducible.swift
+++ ios/SecondHand/SecondHand/Network/URLRequestProducible.swift
@@ -11,6 +11,8 @@ protocol URLRequestProducible {
     func makeUrlRequest(_ urlString: String, query: [String: String], httpMethod: HttpMethod) throws -> URLRequest
     
     func makeUrlRequest(_ urlString: String, query: [String: String], header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest
+    
+    func makeUrlRequest(_ urlComponents: URLComponents, header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest
 }
 
 extension URLRequestProducible {
@@ -43,6 +45,25 @@ extension URLRequestProducible {
         // 메소드 수정
         request.httpMethod = httpMethod.rawValue
         return request
+    }
+    
+    func makeUrlRequest(_ urlComponents: URLComponents, header: [String: String], body: [String: String], httpMethod: HttpMethod) throws -> URLRequest {
+        guard let url = urlComponents.url else { throw URLRequestError.badUrl }
+        var urlRequest = URLRequest(url: url, timeoutInterval: 5.0)
+        
+        // 헤더 수정
+        for (key, value) in header {
+            urlRequest.setValue(value, forHTTPHeaderField: key)
+        }
+        
+        // 바디 수정
+        let jsonData = try? JSONSerialization.data(withJSONObject: body)
+        urlRequest.httpBody = jsonData
+        
+        // 메소드 수정
+        urlRequest.httpMethod = httpMethod.rawValue
+        
+        return urlRequest
     }
 }
 
