02c250c4c07f8458d37ad0636dfc4e3750d820f9
leegyeongwhan <leekhy02@naver.com>
2023-07-18 13:01:16 +0900

    ✨ feat: 로그인 관련 예외 추가

--- be/src/main/java/com/secondhand/domain/exception/CustomizedResponseEntityExceptionHandler.java
+++ be/src/main/java/com/secondhand/domain/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,11 +1,9 @@
 package com.secondhand.domain.exception;
 
-import org.joda.time.LocalDate;
-import org.joda.time.LocalDateTime;
+import com.secondhand.oauth.exception.GitHubRequestException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,21 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(Exception.class)
-    public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
-        ExceptionResponse exceptionResponse =
-                new ExceptionResponse(e.getMessage(), request.getDescription(false));
-
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-    }
+//    @ExceptionHandler(Exception.class)
+//    public final ResponseEntity<Object> handleAllExceptions(Exception e, WebRequest request) {
+//        ExceptionResponse exceptionResponse =
+//                new ExceptionResponse(e.getMessage(), request.getDescription(false));
+//
+//        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+//    }
 
     @ExceptionHandler(ImageUploadFailException.class)
     public final ResponseEntity<Object> handleAllRuntimeExceptions(ImageUploadFailException e, WebRequest request) {
@@ -106,5 +100,21 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new ExceptionResponse(e.getMessage(), request.getDescription(false));
 
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(GitHubRequestException.class)
+    public final ResponseEntity<Object> handleAllGitHubRequestExceptions(GitHubRequestException e, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(e.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(JoinException.class)
+    public final ResponseEntity<Object> handleAllJoinExceptionExceptions(JoinException e, WebRequest request) {
+        ExceptionResponse exceptionResponse =
+                new ExceptionResponse(e.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
