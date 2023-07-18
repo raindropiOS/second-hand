5fb52ca942d8428f090f69b60bba9197cd6ad942
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-18 14:19:14 +0900

    ✨ feat #275: EmailInputViewController UI 작성
    - 이메일 입력란
    - 회원가입 완료 버튼

--- ios/SecondHand/SecondHand/Profile/EmailInputViewController.swift
+++ ios/SecondHand/SecondHand/Profile/EmailInputViewController.swift
@@ -5,4 +5,64 @@
 //  Created by 에디 on 2023/07/18.
 //
 
-import Foundation
+import UIKit
+
+class EmailInputViewController: UIViewController {
+    private lazy var topBottomPadding: CGFloat = self.view.frame.height * 80/852
+    private let separatorViewUnderNavigationBar: SeparatorView = SeparatorView()
+    private let emailInputView: InputView = {
+       let inputView = InputView()
+        
+        return inputView
+    }()
+    private let signUpButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("회원가입 완료", for: .normal)
+        button.backgroundColor = .orange
+        return button
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.configureSeparatorViewUnderNavigationBar()
+        self.configureIdInputView()
+        self.configureSignUpButton()
+    }
+    
+    private func configureSeparatorViewUnderNavigationBar() {
+        self.view.addSubview(separatorViewUnderNavigationBar)
+        self.separatorViewUnderNavigationBar.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            self.separatorViewUnderNavigationBar.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.separatorViewUnderNavigationBar.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor),
+            self.separatorViewUnderNavigationBar.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor),
+            self.separatorViewUnderNavigationBar.heightAnchor.constraint(equalToConstant: SeparatorView.height)
+        ])
+        self.separatorViewUnderNavigationBar.configure()
+    }
+    
+    private func configureIdInputView() {
+        self.view.addSubview(self.emailInputView)
+        emailInputView.configureText(labelText: "이메일", textFieldPlaceholder: "이메일을 입력하세요.")
+        
+        self.emailInputView.translatesAutoresizingMaskIntoConstraints = false
+        let padding = self.topBottomPadding
+        
+        NSLayoutConstraint.activate([
+            emailInputView.topAnchor.constraint(equalTo: separatorViewUnderNavigationBar.bottomAnchor, constant: padding),
+            emailInputView.leadingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leadingAnchor)
+        ])
+    }
+    
+    private func configureSignUpButton() {
+        self.view.addSubview(self.signUpButton)
+        self.signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            self.signUpButton.widthAnchor.constraint(equalToConstant: 330),
+            self.signUpButton.heightAnchor.constraint(equalToConstant: 40),
+            self.signUpButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            self.signUpButton.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -self.topBottomPadding)
+        ])
+    }
+}
