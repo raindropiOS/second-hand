0336cbf025aae5e4ee3625b8ed4cfcee1ca1af18
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-24 11:34:27 +0900

    🐛 fix #259: 토큰 저장 방식 수정

--- ios/SecondHand/SecondHand/SupportingFiles/SceneDelegate.swift
+++ ios/SecondHand/SecondHand/SupportingFiles/SceneDelegate.swift
@@ -34,7 +34,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                         let jwtToken = dto.data.jwtToken
                         let accessToken = jwtToken.accessToken
                         let refreshToken = jwtToken.refreshToken
-                        let tempToken = accessToken + "." + refreshToken
+                        let tempToken = accessToken + "*" + refreshToken
                         let jwt = JWT(token: tempToken)
                         keychainManager.temporarySavedJwt = jwt
                     } else {
