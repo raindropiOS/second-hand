c1c82df999046de581e5960d6fcab13e19d4267b
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-25 14:21:56 +0900

    🐛 fix #259: URLComponentsProducible 파라미터명 변경
    - 의도에 맞게 query로 변경

--- ios/SecondHand/SecondHand/Network/URLComponentsProducible.swift
+++ ios/SecondHand/SecondHand/Network/URLComponentsProducible.swift
@@ -8,18 +8,18 @@
 import Foundation
 
 protocol URLComponentsProducible {
-    func makeUrlComponents(baseUrl: String, path: String, parameters: [String: String]) throws -> URLComponents
+    func makeUrlComponents(baseUrl: String, path: String, query: [String: String]) throws -> URLComponents
 }
 
 extension URLComponentsProducible {
-    func makeUrlComponents(baseUrl: String, path: String, parameters: [String: String]) throws -> URLComponents {
+    func makeUrlComponents(baseUrl: String, path: String, query: [String: String]) throws -> URLComponents {
         guard var urlComponents = URLComponents(string: baseUrl) else { throw UrlComponentsError.failedToMakeUrlComponents}
         
         // Path 반영
         urlComponents.path = path
         
-        // Parameter 수정
-        let queryItems = parameters.map { URLQueryItem(name: $0.key, value: $0.value) }
+        // Query 반영
+        let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
         urlComponents.queryItems = queryItems
         
         return urlComponents
