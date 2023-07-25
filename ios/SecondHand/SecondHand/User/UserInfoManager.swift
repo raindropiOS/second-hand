3929379924477603063f89052f84b7a92e513b7d
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-25 22:19:46 +0900

    ♻️ refactor #274: UserInfoManager 역할 수정
    - 뷰에 필요한 데이터만 담을 수 있도록 수정

--- ios/SecondHand/SecondHand/User/UserInfoManager.swift
+++ ios/SecondHand/SecondHand/User/UserInfoManager.swift
@@ -8,20 +8,9 @@
 import Combine
 import Foundation
 
+/// 뷰에 필요한 유저 정보를 저장하는 Repository Layer
 class UserInfoManager: URLRequestProducible, URLComponentsProducible {
     static let shared = UserInfoManager()
-    private let keychainManagerDelegate: KeychainManageable?
-    private var jwt: JWT?
     @Published var isSignedIn = false
-    
-    init() {
-        self.keychainManagerDelegate = KeychainManager()
-    }
-    
-    func readJsonWebToken() async throws {
-        if let jwt = try await self.keychainManagerDelegate?.readJsonWebToken() {
-            self.jwt = jwt
-            self.isSignedIn = true
-        }
-    }
+    var userInfo: UserInfo?
 }
