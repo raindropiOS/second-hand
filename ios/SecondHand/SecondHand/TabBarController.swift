22cef4bdd26cd0e874b3496baec4056a3063b35c
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-25 18:39:15 +0900

    ✨ feat #274: TabBarController 로그인 상태에 따른 뷰 변경
    - 로그인 상태에 따라 '내 계정'탭 뷰 컨트롤러 변경 기능 추가

--- ios/SecondHand/SecondHand/TabBarController.swift
+++ ios/SecondHand/SecondHand/TabBarController.swift
@@ -5,13 +5,23 @@
 //  Created by 에디 on 2023/06/08.
 //
 
+import Combine
 import UIKit
 
 class TabBarController: UITabBarController {
+    var cancellables = Set<AnyCancellable>()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tabBar.backgroundColor = UIColor(named: "gray200")
+        
+        UserInfoManager.shared.$isSignedIn
+            .sink { [weak self] newData in
+                if newData == true {
+                    self?.updateProfileView()
+                }
+            }
+            .store(in: &cancellables)
         setTabViewControllers()
     }
     
@@ -50,6 +60,17 @@ class TabBarController: UITabBarController {
             
             items[4].title = "내 계정"
             items[4].image = UIImage(systemName: "person")
+        }
+    }
+    
+    private func updateProfileView() {
+        DispatchQueue.main.async {
+            self.viewControllers?[4] = ProfileViewController()
+            
+            if let items = self.tabBar.items {
+                items[4].title = "내 계정"
+                items[4].image = UIImage(systemName: "person")
+            }
         }
     }
 }
