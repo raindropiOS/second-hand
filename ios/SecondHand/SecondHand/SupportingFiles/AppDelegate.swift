5649d162584b6124e83e2de3eab5c8a59e19042c
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-25 18:40:17 +0900

    ✨ feat #274: 앱이 실행될 때 JWT확인하는 기능 추가

--- ios/SecondHand/SecondHand/SupportingFiles/AppDelegate.swift
+++ ios/SecondHand/SecondHand/SupportingFiles/AppDelegate.swift
@@ -9,5 +9,16 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // 앱이 시작될 때 수행할 초기화 작업
+        Task {
+            do {
+                try await UserInfoManager.shared.readJsonWebToken()
+            } catch {
+                print("error: \(error)")
+            }
+        }
+        
+        return true
+    }
 }
