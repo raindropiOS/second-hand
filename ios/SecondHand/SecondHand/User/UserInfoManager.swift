e5a3c194e0b84238196a071ecfebba74a8b11194
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-25 18:37:57 +0900

    ✨ feat #274: UserInfoManager 로그인 기능 추가
    - 키체인에 저장된 JWT를 읽어 로그인 상태 판별하는 기능 추가

--- ios/SecondHand/SecondHand/User/UserInfoManager.swift
+++ ios/SecondHand/SecondHand/User/UserInfoManager.swift
@@ -5,4 +5,23 @@
 //  Created by 에디 on 2023/07/25.
 //
 
+import Combine
 import Foundation
+
+class UserInfoManager: URLRequestProducible, URLComponentsProducible {
+    static let shared = UserInfoManager()
+    private let keychainManagerDelegate: KeychainManageable?
+    private var jwt: JWT?
+    @Published var isSignedIn = false
+    
+    init() {
+        self.keychainManagerDelegate = KeychainManager()
+    }
+    
+    func readJsonWebToken() async throws {
+        if let jwt = try await self.keychainManagerDelegate?.readJsonWebToken() {
+            self.jwt = jwt
+            self.isSignedIn = true
+        }
+    }
+}
