72e4d0c85607d0d24373a063d526f6dabb493c62
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-26 15:50:21 +0900

    🐛 fix #274: makeUrlComponents 수정
    - path가 없는 경우 버그 수정

--- ios/SecondHand/SecondHand/Network/URLComponentsProducible.swift
+++ ios/SecondHand/SecondHand/Network/URLComponentsProducible.swift
@@ -16,7 +16,9 @@ extension URLComponentsProducible {
         guard var urlComponents = URLComponents(string: baseUrl) else { throw UrlComponentsError.failedToMakeUrlComponents}
         
         // Path 반영
-        urlComponents.path = path
+        if path != "" {
+            urlComponents.path = path
+        }
         
         // Query 반영
         let queryItems = query.map { URLQueryItem(name: $0.key, value: $0.value) }
