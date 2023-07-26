e4906aabc277ea0c5d012f955031c45a0329ed49
JaydenLee1116 <vv55adss@gmail.com>
2023-07-26 12:26:23 +0900

    ♻️ refactor: 로그인 페이지 사용하지 않는 코드 제거
    - 기존 카카오 지도 api로 현재 동네 보여주는 기능 코드 제거
    - handleLoginBtn을 LoginMain 컴포넌트로 옮김

--- fe/src/pages/Login/index.tsx
+++ fe/src/pages/Login/index.tsx
@@ -1,9 +1,4 @@
-import React, { useState, useEffect } from 'react';
-
-import { AUTH, KAKAO } from '@constants/API';
-
-import kakaoFetch from 'src/apis/instances/kakaoFetch';
-import useCurrentRegion from '@hooks/useCurrentRegion';
+import React from 'react';
 
 import LoginHeader from '@components/Login/LoginHeader';
 import LoginMain from '@components/Login/LoginMain';
@@ -12,19 +7,10 @@ import { $Template } from '@styles/PageTemplate.style';
 import { Outlet } from 'react-router-dom';
 
 const Login = () => {
-  const { region, error } = useCurrentRegion();
-
-  const handleLoginBtnClick = () => {
-    const scope = 'user';
-    const clientId = process.env.REACT_APP_CLIENT_ID as string;
-
-    window.location.href = AUTH.GITHUB_LOGIN_URL(clientId, scope);
-  };
-
   return (
     <$Template>
       <LoginHeader />
-      <LoginMain onClick={handleLoginBtnClick} region={region} />
+      <LoginMain />
       <Outlet />
     </$Template>
   );
