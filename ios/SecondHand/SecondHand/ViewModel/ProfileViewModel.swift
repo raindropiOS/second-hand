a1a0ac254a80bfafd303dbba751e35cb4d80cbba
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-26 14:30:49 +0900

    ✨ feat #274: ProfileViewModel 추가

---
+++ ios/SecondHand/SecondHand/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewModel.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/07/26.
+//
+
+import Foundation
+
+struct ProfileViewModel {
+    let profileImageUrlString: String
+    let userName: String
+}
