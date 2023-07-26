38bb3e92491196c7ca9d066d546c167e976e04b9
JaydenLee1116 <vv55adss@gmail.com>
2023-07-26 12:23:46 +0900

    ✨ feat: 카카오 로그인 버튼 및 API 추가

--- fe/src/components/Login/LoginMain/index.tsx
+++ fe/src/components/Login/LoginMain/index.tsx
@@ -1,23 +1,37 @@
 import React from 'react';
 
-import { $LoginMain, $LoginButton, $ProfileImage } from './LoginMain.style';
+import { $LoginMain, $LoginButtonWrapper, $LoginButton, $ProfileImage } from './LoginMain.style';
 import Icon from '@atoms/Icon';
+import { AUTH } from '@constants/API';
 
-interface LoginMainProps {
-  onClick: () => void;
-  region: string;
-}
+const LoginMain = () => {
+  const handleKaKaoLoginBtnClick = () => {
+    const clientId = process.env.REACT_APP_OAUTH_KAKAO_CLIENT_ID as string;
 
-const LoginMain = ({ onClick, region }: LoginMainProps) => {
+    window.location.href = AUTH.KAKAO_LOGIN_URL(clientId);
+  };
+  const handleGithubLoginBtnClick = () => {
+    const scope = 'user';
+    const clientId = process.env.REACT_APP_OAUTH_GITHUB_CLIENT_ID as string;
+
+    window.location.href = AUTH.GITHUB_LOGIN_URL(clientId, scope);
+  };
+
   return (
     <$LoginMain>
       <$ProfileImage>
         <Icon name="camera" width={40} height={35} />
       </$ProfileImage>
-      <$LoginButton onClick={onClick}>
-        <Icon name="github" width={40} height={40} />
-        깃허브로 로그인하기
-      </$LoginButton>
+      <$LoginButtonWrapper>
+        <$LoginButton onClick={handleKaKaoLoginBtnClick} color="#000" backgroundColor="#f7e10d">
+          <Icon name="kakao" width={40} height={40} />
+          카카오로 로그인하기
+        </$LoginButton>
+        <$LoginButton onClick={handleGithubLoginBtnClick} color="#fff" backgroundColor="#000">
+          <Icon name="github" width={40} height={40} />
+          깃허브로 로그인하기
+        </$LoginButton>
+      </$LoginButtonWrapper>
     </$LoginMain>
   );
 };
