1a5990dbae242c9a2ed7f08ecd4747932e32f47a
JaydenLee1116 <vv55adss@gmail.com>
2023-07-27 11:59:20 +0900

    ✨ feat: 카카오 로그아웃 기능 추가

--- fe/src/components/Login/LoginMain/index.tsx
+++ fe/src/components/Login/LoginMain/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { $LoginMain, $LoginButtonWrapper, $LoginButton, $ProfileImage } from './LoginMain.style';
 import Icon from '@atoms/Icon';
 import { AUTH } from '@constants/API';
 
 const LoginMain = () => {
+  const [accessToken, setAccessToken] = useState<string | null>(sessionStorage.getItem('accessToken'));
+
   const handleKaKaoLoginBtnClick = () => {
     const clientId = process.env.REACT_APP_OAUTH_KAKAO_CLIENT_ID as string;
 
@@ -16,6 +18,13 @@ const LoginMain = () => {
 
     window.location.href = AUTH.GITHUB_LOGIN_URL(clientId, scope);
   };
+  const handleKaKaoLogoutBtnClick = () => {
+    setAccessToken(null);
+    sessionStorage.removeItem('accessToken');
+    sessionStorage.removeItem('refreshToken');
+    sessionStorage.removeItem('name');
+    sessionStorage.removeItem('imgUrl');
+  };
 
   return (
     <$LoginMain>
@@ -23,14 +32,23 @@ const LoginMain = () => {
         <Icon name="camera" width={40} height={35} />
       </$ProfileImage>
       <$LoginButtonWrapper>
-        <$LoginButton onClick={handleKaKaoLoginBtnClick} color="#000" backgroundColor="#f7e10d">
-          <Icon name="kakao" width={40} height={40} />
-          카카오로 로그인하기
-        </$LoginButton>
-        <$LoginButton onClick={handleGithubLoginBtnClick} color="#fff" backgroundColor="#000">
-          <Icon name="github" width={40} height={40} />
-          깃허브로 로그인하기
-        </$LoginButton>
+        {sessionStorage.getItem('accessToken') ? (
+          <$LoginButton onClick={handleKaKaoLogoutBtnClick} color="#000" backgroundColor="#f7e10d">
+            <Icon name="kakao" width={40} height={40} />
+            카카오 로그아웃하기
+          </$LoginButton>
+        ) : (
+          <>
+            <$LoginButton onClick={handleKaKaoLoginBtnClick} color="#000" backgroundColor="#f7e10d">
+              <Icon name="kakao" width={40} height={40} />
+              카카오로 로그인하기
+            </$LoginButton>
+            <$LoginButton onClick={handleGithubLoginBtnClick} color="#fff" backgroundColor="#000">
+              <Icon name="github" width={40} height={40} />
+              깃허브로 로그인하기
+            </$LoginButton>
+          </>
+        )}
       </$LoginButtonWrapper>
     </$LoginMain>
   );
