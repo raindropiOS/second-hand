af62e56918e5c4f4352c048f15d1ab8708accdf9
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-07-27 17:31:24 +0900

    ♻️ refactor #274: LoginManager 의존성 주입

--- ios/SecondHand/SecondHand/Network/NetworkManager.swift
+++ ios/SecondHand/SecondHand/Network/NetworkManager.swift
@@ -13,10 +13,16 @@ class NetworkManager: NetworkManageable, URLRequestable, URLRequestProducible, U
     private let dataDecoder: DataDecodable = DataDecoder()
     private let baseUrlString = Bundle.main.infoDictionary?["baseUrl"] as? String ?? ""
     private let basePath = "/api"
-    private let clientId = Bundle.main.infoDictionary?["githubClientId"] as? String ?? ""
-    private let githubBaseUrl = "https://github.com"
-    private lazy var githubOAuthParameters = ["client_id": self.clientId, "scope": "user public_repo"]
+    private let loginManagerDelegate: LoginManageable
     var jwt: JWT?
+    
+    init(loginManagerDelegate: LoginManageable) {
+        self.loginManagerDelegate = loginManagerDelegate
+    }
+    
+    convenience init() {
+        self.init(loginManagerDelegate: LoginManager())
+    }
     
     func fetchProducts(query: [String: String]) async -> [Product] {
         do {
@@ -79,16 +85,7 @@ class NetworkManager: NetworkManageable, URLRequestable, URLRequestProducible, U
  */
 extension NetworkManager {
     func presentGithubOAuthLoginScreen() {
-        let path = "/login/oauth/authorize"
-        
-        do {
-            let urlComponents = try self.makeUrlComponents(baseUrl: self.githubBaseUrl, path: path, query: self.githubOAuthParameters)
-            if let url = urlComponents.url {
-                UIApplication.shared.open(url)
-            }
-        } catch {
-            print("error : \(error)")
-        }
+        self.loginManagerDelegate.presentGithubOAuthLoginScreen()
     }
     
     func getQueryItemValue(urlString: String, key: String) throws -> String {
@@ -100,46 +97,12 @@ extension NetworkManager {
     }
     
     func sendAuthorizationCode(_ code: String) async throws -> GitHubOAuthResponseDTO {
-        let body = ["authorizationCode": "\(code)"]
-        let header = ["Content-Type": "application/json"]
-        
-        do {
-            let urlComponents = try self.makeUrlComponents(baseUrl: self.baseUrlString, path: "/api/auth/github/login", query: [:])
-            let urlRequest = try self.makeUrlRequest(urlComponents, header: header, body: body, httpMethod: .post)
-            let data = try await self.fetchData(with: urlRequest)
-            let reponseData = try self.dataDecoder.decodeJSON(data, DTO: GitHubOAuthResponseDTO.self)
-            return reponseData
-        } catch {
-            print("error : \(error)")
-            throw NetworkingError.failedToSendAuthorizationCode
-        }
+        try await self.loginManagerDelegate.sendAuthorizationCode(code)
     }
     
     /// GitHub OAuth 인증코드를 전달한 뒤, 이메일을 보내는 작업을 수행하는 메소드
     func sendEmail(_ email: String, jwtAccessToken: String) async throws -> (UserInfo, JWT) {
-        do {
-            let urlComponents = try self.makeUrlComponents(
-                baseUrl: self.baseUrlString,
-                path: "/api/members/signup",
-                query: [:])
-            let urlRequest = try self.makeUrlRequest(
-                urlComponents,
-                header: [
-                    "Authorization": "Bearer\(jwtAccessToken)",
-                    "Content-Type": "application/json",
-                ],
-                body: ["email": "\(email)"],
-                httpMethod: .post)
-            let userData = try await self.fetchData(with: urlRequest)
-            let dto = try self.dataDecoder.decodeJSON(userData,
-                                                              DTO: GitHubOAuthResponseDTO.self)
-            let userInfo = UserInfo(name: dto.data.name, profileImageUrlString: dto.data.imgUrl)
-            let jwt = JWT(accessToken: dto.data.jwtToken.accessToken,
-                          refreshToken: dto.data.jwtToken.refreshToken)
-            return (userInfo, jwt)
-        } catch {
-            throw NetworkingError.failedToSendEmail
-        }
+        try await self.loginManagerDelegate.sendEmail(email, jwtAccessToken: jwtAccessToken)
     }
     
     func fetchProfileInfo() async throws -> UserInfo {
