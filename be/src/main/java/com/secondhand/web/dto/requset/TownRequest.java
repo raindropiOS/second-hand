728e6f0be8bc19eafd4083167127740db78d9d7d
leegyeongwhan <leekhy02@naver.com>
2023-07-28 15:01:16 +0900

    ✨ feat: 회원 타운 수정 코드 수정

--- be/src/main/java/com/secondhand/web/dto/requset/TownRequest.java
+++ be/src/main/java/com/secondhand/web/dto/requset/TownRequest.java
@@ -1,16 +1,10 @@
 package com.secondhand.web.dto.requset;
 
 import lombok.Getter;
-import reactor.util.annotation.Nullable;
-
-import javax.validation.constraints.NotNull;
 
 
 @Getter
 public class TownRequest {
 
-    @NotNull
-    private Long mainTownId;
-    @Nullable
-    private Long subTownId;
+    private Long[] townsId;
 }
