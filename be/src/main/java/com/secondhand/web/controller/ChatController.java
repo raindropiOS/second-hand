5970e1e398fc1f4cc08a83a8f676ba6e61fa6fe9
leegyeongwhan <leekhy02@naver.com>
2023-07-29 21:33:40 +0900

    🎨 style : requestmapping 추가

--- be/src/main/java/com/secondhand/web/controller/ChatController.java
+++ be/src/main/java/com/secondhand/web/controller/ChatController.java
@@ -3,11 +3,13 @@ package com.secondhand.web.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/chat")
 public class ChatController {
 
     @MessageMapping("/message")
