f56937cee21e3727005e9140bb21b9d10243d105
harimrim <kigkfla@naver.com>
2023-07-31 22:54:02 +0900

    ♻️ refactor #295 : TabBarController 구현 부분 제거

--- ios/SecondHand/SecondHand/TabBarController.swift
+++ ios/SecondHand/SecondHand/TabBarController.swift
@@ -11,45 +11,6 @@ class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBar.backgroundColor = UIColor(named: "gray200")
-        setTabViewControllers()
-    }
-    
-    private func setTabViewControllers() {
-        let pastTimeCalculator: PastTimeCalculable = PastTimeCalculator()
-        let productListViewModel = ProductListViewModel(productRepository: ProductRepository(), pastTimeCalculator: pastTimeCalculator)
-        
-        let homeViewController = UINavigationController(rootViewController: HomeViewController(productListViewModel: productListViewModel))
-        let salesLogViewController = SalesLogViewController()
-        let likeListViewController = LikeListViewController()
-        let chattingViewController = ChattingViewController()
-        let signInViewController = UINavigationController(rootViewController: SignInViewController())
-        
-        let viewControllers = [
-            homeViewController,
-            salesLogViewController,
-            likeListViewController,
-            chattingViewController,
-            signInViewController
-        ]
-        
-        self.setViewControllers(viewControllers, animated: false)
-        
-        if let items = self.tabBar.items {
-            items[0].title = "홈"
-            items[0].image = UIImage(systemName: "house")
-            
-            items[1].title = "판매내역"
-            items[1].image = UIImage(systemName: "newspaper")
-            
-            items[2].title = "관심목록"
-            items[2].image = UIImage(systemName: "heart")
-            
-            items[3].title = "채팅"
-            items[3].image = UIImage(systemName: "message")
-            
-            items[4].title = "내 계정"
-            items[4].image = UIImage(systemName: "person")
-        }
+
     }
 }
