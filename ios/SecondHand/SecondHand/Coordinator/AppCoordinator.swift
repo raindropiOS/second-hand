8ea611792126c5b6733664e1f3b64e370d936e0a
harimrim <kigkfla@naver.com>
2023-07-31 22:39:04 +0900

    ✨ feat #297 : AppCoordinator 구현

--- ios/SecondHand/SecondHand/Coordinator/AppCoordinator.swift
+++ ios/SecondHand/SecondHand/Coordinator/AppCoordinator.swift
@@ -6,3 +6,43 @@
 //
 
 import UIKit
+
+class AppCoordinator: NSObject, Coordinator {
+    
+    // MARK: Properties
+    
+    var delegate: CoordinatorFinishDelegate?
+    
+    let window: UIWindow
+    
+    var presenter: UINavigationController
+
+    var childCoordinators: [Coordinator]
+    
+    var tabBarController: UITabBarController
+    
+    
+    // MARK: Initializing
+    
+    init(window: UIWindow) {
+        self.window = window
+        
+        let navigationController = UINavigationController()
+        navigationController.setNavigationBarHidden(true, animated: true)
+        self.presenter = navigationController
+        
+        self.childCoordinators = []
+        self.tabBarController = UITabBarController()
+    }
+    
+    func start() {
+        window.rootViewController = presenter
+        let coordinator = TabBarCoordinator(presenter: presenter)
+        coordinator.delegate = self
+        childCoordinators.append(coordinator)
+        coordinator.start()
+        
+        window.makeKeyAndVisible()
+    }
+    
+}
