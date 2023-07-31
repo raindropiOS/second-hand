8d82ecf10a512908959b079bc4810b5def3da87f
harimrim <kigkfla@naver.com>
2023-07-31 22:32:55 +0900

    ✨ feat #296 : Coordinator Protocol 구현
    - childCoordinator들을 관리할 배열
    - ViewController를 Push 혹으 Pop할 수 있는 UInavigationController
    - App을 관리할 준비가 되었을 때 호출되는 start 메서드
    - Coordinator의 역할이 끝났음을 알리는 CoordinatorFinishDelegate

--- ios/SecondHand/SecondHand/Coordinator/Coordinator.swift
+++ ios/SecondHand/SecondHand/Coordinator/Coordinator.swift
@@ -8,3 +8,13 @@
 import UIKit
 
 
+protocol Coordinator: AnyObject, CoordinatorFinishDelegate {
+    
+    var delegate: CoordinatorFinishDelegate? { get set }
+    
+    var presenter: UINavigationController { get set }
+    
+    var childCoordinators: [Coordinator] { get set }
+    
+    func start()
+}
