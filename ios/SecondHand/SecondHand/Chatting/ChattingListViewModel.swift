6251a468c95b3337da4725c6c10104a005a57416
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-08-07 16:42:56 +0900

    ✨ feat #311: ChattingListViewModel 추가
    - 채팅 목록 뷰 컨트롤러에 사용할 뷰 모델 추가

---
+++ ios/SecondHand/SecondHand/Chatting/ChattingListViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingListViewModel.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/08/07.
+//
+
+import Foundation
+
+struct ChattingListViewModel {
+    let chattingList: [ChattingListTableViewCellViewModel]
+}
