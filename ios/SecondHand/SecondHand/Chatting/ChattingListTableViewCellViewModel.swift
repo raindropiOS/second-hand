d7c43055b457d6e1e6c97abd144c87a57af55e2e
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-08-07 15:44:48 +0900

    ✨ feat #311: ChattingListTableViewCellViewModel 파일 추가

---
+++ ios/SecondHand/SecondHand/Chatting/ChattingListTableViewCellViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ChattingListTableViewCellViewModel.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/08/07.
+//
+
+import Foundation
+
+class ChattingListTableViewCellViewModel {
+    
+}
