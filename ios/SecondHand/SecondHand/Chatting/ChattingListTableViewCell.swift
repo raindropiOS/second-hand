3cbd30df463945c040bf232780a7a437dea5808c
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-08-07 15:43:05 +0900

    ✨ feat #311: ChattingListTableViewCell 파일 추가

---
+++ ios/SecondHand/SecondHand/Chatting/ChattingListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingListTableViewCell.swift
+//  SecondHand
+//
+//  Created by 에디 on 2023/08/07.
+//
+
+import UIKit
+
+class ChattingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
