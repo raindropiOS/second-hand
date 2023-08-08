b4ef6ed8070e1e3002233a813fc8ed1a68557bd6
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-08-08 11:14:28 +0900

    ✨ feat #312: ChattingViewController init 작성

--- ios/SecondHand/SecondHand/Chatting/ChattingViewController.swift
+++ ios/SecondHand/SecondHand/Chatting/ChattingViewController.swift
@@ -5,4 +5,22 @@
 //  Created by 에디 on 2023/08/08.
 //
 
-import Foundation
+import UIKit
+
+class ChattingViewController: UIViewController {
+    let viewModel: ChattingViewModel
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+    }
+    
+    init(viewModel: ChattingViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
